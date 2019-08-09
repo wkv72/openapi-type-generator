e8adbc28bb1ab8b1a7dfae7b81c91228736b45cb
Werner Vesterås <werner.vesteraas@marcello.no>
2019-08-09 12:50:37 +0200

    -Added validation

--- dist/index.js
+++ dist/index.js
@@ -17,17 +17,21 @@ const path_1 = __importDefault(require("path"));
 const utils_1 = require("./utils");
 const parsedArgs = minimist(process.argv.slice(2));
 if (!parsedArgs.i) {
-    console.error("The -i argument is missing.");
+    console.error("[Error] Required option '-i' is missing.");
     process.exit(1);
 }
 if (!parsedArgs.o) {
     // tslint:disable-next-line:no-console
-    console.error("The -o argument is missing.");
+    console.error("[Error] Required option '-o' is missing.");
     process.exit(1);
 }
 let force = false;
 if (parsedArgs.f) {
     force = true;
+}
+let validate = false;
+if (parsedArgs.v) {
+    validate = true;
 }
 const inputFile = path_1.default.join(process.cwd(), parsedArgs.i);
 const outputDir = path_1.default.join(process.cwd(), parsedArgs.o);
@@ -35,12 +39,21 @@ function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             if (!(yield utils_1.fileExists(inputFile))) {
-                console.error(`Input file '${inputFile}' does not exist.`);
+                console.error(`[Error] The input file '${inputFile}' does not exist.`);
                 process.exit(2);
             }
             if (!force && (yield utils_1.fileExists(outputDir))) {
-                console.error(`Output directory '${outputDir}' already exist.`);
+                console.error(`[Error] The output directory '${outputDir}' already exist.`);
                 process.exit(3);
+            }
+            if (validate) {
+                try {
+                    yield utils_1.execute(`openapi-generator validate -i ${inputFile}`);
+                }
+                catch (err) {
+                    console.error(`[Error] The input file '${inputFile}' failed validation.`);
+                    process.exit(4);
+                }
             }
             const tempDir = yield utils_1.createTempDir("openapi-tg");
             yield utils_1.execute(`openapi-generator generate -g typescript-node -i ${inputFile} -o ${tempDir}`);
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ import {copyDir, createTempDir, execute, fileExists} from "./utils";
 const parsedArgs = minimist(process.argv.slice(2));
 
 if (!parsedArgs.i) {
-    console.error("The -i argument is missing.");
+    console.error("[Error] Required option '-i' is missing.");
     process.exit(1);
 }
 
 if (!parsedArgs.o) {
     // tslint:disable-next-line:no-console
-    console.error("The -o argument is missing.");
+    console.error("[Error] Required option '-o' is missing.");
     process.exit(1);
 }
 
@@ -24,19 +24,34 @@ if (parsedArgs.f) {
     force = true;
 }
 
+let validate = false;
+
+if (parsedArgs.v) {
+    validate = true;
+}
+
 const inputFile = path.join(process.cwd(), parsedArgs.i);
 const outputDir = path.join(process.cwd(), parsedArgs.o);
 
 async function main(): Promise<void> {
     try {
         if (!await fileExists(inputFile)) {
-            console.error(`Input file '${inputFile}' does not exist.`);
+            console.error(`[Error] The input file '${inputFile}' does not exist.`);
             process.exit(2);
         }
 
         if (!force && await fileExists(outputDir)) {
-            console.error(`Output directory '${outputDir}' already exist.`);
+            console.error(`[Error] The output directory '${outputDir}' already exist.`);
             process.exit(3);
+        }
+
+        if (validate) {
+            try {
+                await execute(`openapi-generator validate -i ${inputFile}`);
+            } catch (err) {
+                console.error(`[Error] The input file '${inputFile}' failed validation.`);
+                process.exit(4);
+            }
         }
 
         const tempDir = await createTempDir("openapi-tg");
